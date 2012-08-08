36f03b6c9817f3193a902bf7c0a5a1e17ea7bfe6
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-08-08 08:51:30 -0500

    added the manifest

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,2 +1,15 @@
+
+
 # Make sure you list all the project template files here in the manifest.
-stylesheet 'screen.sass', :media => 'screen, projection'
+stylesheet '_framed-scss.scss', :media => 'screen, projection'
+# html 'framed.html'
+
+description "structural foundations."
+
+help %Q{
+Framed, SCSS version.
+}
+
+welcome_message %Q{
+Framed, SCSS version.
+}
